6f547f3fd0f225f08ee48aa67e0126d61edbe4c8
Guru <rjguru1111@gmail.com>
2022-08-29 13:02:40 +0530

    Added Get Username call

---
+++ AL.RMZ/AL.RMZ/Controllers/Security/UserController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AL.RMZ.Controllers.Security
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult CurrentUserName()
+        {
+            return Ok(User.Identity.Name);
+        }
+    }
+}
